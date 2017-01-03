95004b4da31c52272ff5eb5db0f83dbe32ce61f7
DESKTOP-KP0GSC5\heyunchao <yunchaohe@gmail.com>
2017-01-03 14:46:36 +0800

    nn

--- 1_Sentiment_Classification/nn_example_2.py
+++ 1_Sentiment_Classification/nn_example_2.py
@@ -1,17 +1,22 @@
-'''
-A Multilayer Perceptron implementation example using TensorFlow library.
-This example is using the MNIST database of handwritten digits
-(http://yann.lecun.com/exdb/mnist/)
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-'''
-
 from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
+'''
+Use a Neural Network to count the number 1 in a 0-1 list of length 16, and classify the list into 2 classes:
 
-def get_batch_data(batch_size, total_batch):
+[1, 0], if #1 > 8
+[0, 1], if #1 < 8
+
+After the iteration, we can get:
+
+Epoch: 0996 cost= 0.101883064
+Optimization Finished!
+Accuracy: 0.991
+'''
+
+
+def get_batch_data(batch_size):
     while True:
         x_batch = []
         y_batch = []
@@ -19,36 +24,32 @@ def get_batch_data(batch_size, total_batch):
             line = np.random.choice([0, 1], size=(16))
             x_batch.append(line)
             sum = np.sum(line)
-            if sum % 3 == 0:
-                y = [1, 0, 0]
-            elif sum % 3 == 1:
-                y = [0, 1, 0]
-            else:  # 2
-                y = [0, 0, 1]
+            if sum > 8:
+                y = [1, 0]
+            else:
+                y = [0, 1]
             y_batch.append(y)
         x_batch = np.array(x_batch).reshape(batch_size, 16)
-        y_batch = np.array(y_batch).reshape(batch_size, 3)
+        y_batch = np.array(y_batch).reshape(batch_size, 2)
         yield (x_batch, y_batch)
 
 
 # 几乎和training data 加载方式一样
-def get_test_data():
+def get_test_data(size=100):
     # load test data
     x_test = []
     y_test = []
-    for i in range(100):
+    for i in range(size):
         line = np.random.choice([0, 1], size=(16))
         x_test.append(line)
         sum = np.sum(line)
-        if sum % 3 == 0:
-            y = [1, 0, 0]
-        elif sum % 3 == 1:
-            y = [0, 1, 0]
-        else:  # 2
-            y = [0, 0, 1]
+        if sum > 8:
+            y = [1, 0]
+        else:
+            y = [0, 1]
         y_test.append(y)
-    x_test = np.array(x_test).reshape(100, 16)
-    y_test = np.array(y_test).reshape(100, 3)
+    x_test = np.array(x_test).reshape(size, 16)
+    y_test = np.array(y_test).reshape(size, 2)
     return (x_test, y_test)
 
 
@@ -60,15 +61,15 @@ def comput_acc(pred, target):
 
 # Parameters
 learning_rate = 0.001
-training_epochs = 100
+training_epochs = 1000
 batch_size = 5
-display_step = 2
+display_step = 5
 
 # Network Parameters
 n_hidden_1 = 8  # 1st layer number of features
 n_hidden_2 = 8  # 2nd layer number of features
 n_input = 16  # MNIST data input (img shape: 28*28)
-n_classes = 3  # MNIST total classes (0-9 digits)
+n_classes = 2  # MNIST total classes (0-9 digits)
 
 # tf Graph input
 x = tf.placeholder("float", [None, n_input])
@@ -79,10 +80,10 @@ y = tf.placeholder("float", [None, n_classes])
 def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    # layer_1 = tf.nn.relu(layer_1)
+    layer_1 = tf.nn.relu(layer_1)
     # Hidden layer with RELU activation
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    # layer_2 = tf.nn.relu(layer_2)
+    layer_2 = tf.nn.relu(layer_2)
     # Output layer with linear activation
     out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
     return out_layer
@@ -110,9 +111,9 @@ optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 # Initializing the variables
 init = tf.initialize_all_variables()
 
-total_batch = 5
-data = get_batch_data(batch_size, total_batch)
-x_test, y_test = get_test_data()
+total_batch = 10
+data = get_batch_data(batch_size)
+x_test, y_test = get_test_data(1000)
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
@@ -131,6 +132,8 @@ with tf.Session() as sess:
         # Display logs per epoch step
 
         if epoch % display_step == 0:
+            # print(sess.run(pred, feed_dict={x: x_test}))
+            # print(y_test)
             print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
 
             print("Optimization Finished!")
